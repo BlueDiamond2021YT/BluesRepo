8d8c90b3aeeae6cb67602daaa27921e59020262d
BlueDiamond2021YT <139054327+BlueDiamond2021YT@users.noreply.github.com>
2024-11-24 22:03:34 +0000

    Update update_repo_status.py

--- update_repo_status.py
+++ update_repo_status.py
@@ -84,7 +84,7 @@ def update_repo_status(action_status, modified_files):
 
     tint_color = "#00FF00" if action_status == "failure" else "#FF0000"  # Red for failure, green for success
     caption = f"Last repo refresh: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
-    caption += f"List of files modified by last action:\n {', '.join(modified_files)}"
+    caption += f"Last modifed files:\n {', '.join(modified_files)}"
 
     status_info["news"] = [
         {

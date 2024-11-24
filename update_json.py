ee5df6e81ea313eb89fcde66dec0aca98636915e
BlueDiamond2021YT <139054327+BlueDiamond2021YT@users.noreply.github.com>
2024-11-24 11:44:34 +0000

    Update update_json.py

--- update_json.py
+++ update_json.py
@@ -17,36 +17,36 @@ headers = {
     'Accept': 'application/vnd.github.v3+json'
 }
 
-def extract_icon(ipa_path, app_name):
-    # Unzip IPA file
+def extract_icon_and_metadata(ipa_path, app_name):
     with zipfile.ZipFile(ipa_path, 'r') as z:
         payload_path = [name for name in z.namelist() if name.startswith('Payload/') and name.endswith('.app/')][0]
         app_icons = [name for name in z.namelist() if payload_path in name and 'AppIcon' in name and name.endswith('.png')]
+        entitlements = [name for name in z.namelist() if payload_path in name and 'Entitlements.plist' in name]
         
-        # Extract and save the largest icon (assuming it's the main app icon)
+        # Extract the largest icon
         if app_icons:
             largest_icon = max(app_icons, key=lambda x: int(x.split('@')[1].split('x')[0]) if '@' in x else 1)
             icon_data = z.read(largest_icon)
-            
-            # Save to resources/icons/
-            icons_dir = Path('./resources/icons')
+            icons_dir = Path(f'./resources/icons')
             icons_dir.mkdir(parents=True, exist_ok=True)
             icon_path = icons_dir / f"{app_name}_icon.png"
             with open(icon_path, 'wb') as icon_file:
                 icon_file.write(icon_data)
-            
-            print(f"Extracted and saved app icon for {app_name}: {icon_path}")
-            return str(icon_path)
-        else:
-            print(f"No app icons found for {app_name}.")
-            return None
+        
+        # Extract entitlements (if any)
+        permissions = []
+        if entitlements:
+            entitlements_data = z.read(entitlements[0])
+            # Assuming plistlib or similar is used to parse entitlements data
+            # permissions = parse_entitlements(entitlements_data)
+        
+        return str(icon_path), permissions
 
 def process_app(app_config):
     SOURCE_REPO_OWNER = app_config['repo_owner']
     SOURCE_REPO_NAME = app_config['repo_name']
     WORKFLOW_ID = app_config['workflow_id']
     
-    # Fetch the latest workflow runs from the source repository
     response = requests.get(
         f'https://api.github.com/repos/{SOURCE_REPO_OWNER}/{SOURCE_REPO_NAME}/actions/workflows/{WORKFLOW_ID}/runs',
         headers=headers
@@ -58,7 +58,6 @@ def process_app(app_config):
 
     runs = response.json()
 
-    # Get the latest successful run
     try:
         latest_run = next(run for run in runs['workflow_runs'] if run['conclusion'] == 'success')
     except StopIteration:
@@ -67,7 +66,6 @@ def process_app(app_config):
 
     run_id = latest_run['id']
 
-    # Fetch artifacts for the latest successful run from the source repository
     artifacts_response = requests.get(
         f'https://api.github.com/repos/{SOURCE_REPO_OWNER}/{SOURCE_REPO_NAME}/actions/runs/{run_id}/artifacts',
         headers=headers
@@ -86,16 +84,14 @@ def process_app(app_config):
     artifact = artifacts['artifacts'][0]
     artifact_url = artifact['archive_download_url']
 
-    # Download the artifact zip file
     zip_response = requests.get(artifact_url, headers=headers)
     if zip_response.status_code != 200:
         print(f"Failed to download artifact for {app_config['name']}: {zip_response.status_code}")
         return None
 
-    # Create downloads directory if it doesn't exist
-    os.makedirs('./downloads', exist_ok=True)
+    downloads_dir = Path(f'./downloads/{app_config["name"]}')
+    downloads_dir.mkdir(parents=True, exist_ok=True)
 
-    # Extract the .ipa file from the downloaded zip and save it in ./downloads/
     with zipfile.ZipFile(io.BytesIO(zip_response.content)) as z:
         ipa_files = [f for f in z.namelist() if f.endswith('.ipa')]
         
@@ -105,44 +101,48 @@ def process_app(app_config):
         
         ipa_file_name = ipa_files[0]
         
-        # Define version for naming and save path
         version = latest_run['head_commit']['id'][:7]
-        save_path = f"./downloads/{ipa_file_name[:-4]}-{version}.ipa"
+        save_path = downloads_dir / f"{ipa_file_name[:-4]}-{version}.ipa"
 
-        # Extract and save .ipa file to specified path
         with open(save_path, 'wb') as ipa_file:
             ipa_file.write(z.read(ipa_file_name))
 
-        # Extract and save app icon
-        icon_path = extract_icon(save_path, app_config['name'])
+        icon_path, permissions = extract_icon_and_metadata(save_path, app_config['name'])
 
     print(f"Extracted and saved IPA File for {app_config['name']}: {save_path}")
 
-    # Construct the download URL dynamically based on current repo info
-    download_url = f"https://raw.githubusercontent.com/{CURRENT_REPO}/main/downloads/{os.path.basename(save_path)}"
+    download_url = f"https://raw.githubusercontent.com/{CURRENT_REPO}/main/downloads/{app_config['name']}/{os.path.basename(save_path)}"
 
     return {
+        "beta": True,
         "name": app_config['name'],
         "bundleIdentifier": app_config['bundle_identifier'],
+        "developerName": SOURCE_REPO_OWNER,
         "version": version,
         "versionDate": latest_run['created_at'].split('T')[0],
         "versionDescription": f"Latest build from {latest_run['name']}",
         "downloadURL": download_url,
-        "iconURL": f"https://raw.githubusercontent.com/{CURRENT_REPO}/main/resources/icons/{os.path.basename(icon_path)}" if icon_path else None
+        "iconURL": f"https://raw.githubusercontent.com/{CURRENT_REPO}/main/resources/icons/{os.path.basename(icon_path)}",
+        "localizedDescription": "Run iOS app without actually installing it!",
+        "tintColor": "#0784FC",
+        "category": "utilities",
+        "size": os.path.getsize(save_path),
+        "screenshotURLs": [],
+        "appPermissions": {
+            "entitlements": permissions,
+            "privacy": {}
+        }
     }
 
-# Load app configurations
 with open('app_config.json', 'r') as config_file:
     app_configs = json.load(config_file)['apps']
 
-# Process each app
 updated_apps = []
 for app_config in app_configs:
     app_data = process_app(app_config)
     if app_data:
         updated_apps.append(app_data)
 
-# Load existing JSON file and update it with new information
 try:
     with open('sidestore_repo.json', 'r') as file:
         if os.stat('sidestore_repo.json').st_size == 0:
@@ -152,7 +152,6 @@ except (FileNotFoundError, ValueError) as e:
     print(f"Error loading JSON file: {e}")
     exit(1)
 
-# Update or add new apps in the JSON data using unique keys (e.g., name + repo_owner)
 for updated_app in updated_apps:
     unique_key = (updated_app["name"], updated_app["bundleIdentifier"])
     
@@ -160,11 +159,10 @@ for updated_app in updated_apps:
                                if (d["name"], d["bundleIdentifier"]) == unique_key), None)
     
     if existing_app_index is not None:
-        data['apps'][existing_app_index] = updated_app  # Update existing entry
+        data['apps'][existing_app_index] = updated_app
     else:
-        data['apps'].append(updated_app)  # Add new entry
+        data['apps'].append(updated_app)
 
-# Save updated JSON file and print its contents to the console
 try:
     with open('sidestore_repo.json', 'w') as file:
         json.dump(data, file, indent=4)
